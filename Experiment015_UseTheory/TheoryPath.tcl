c9932865d98de5699982c068686cb37c735bfe50
Fredrik Öhrström <oehrstroem@gmail.com>
2020-08-22 11:10:39 +0200

    Rename dirs and add test txt file.

---
+++ Experiment015_UseTheory/TheoryPath.tcl
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<org.eventb.theory.core.scTheoryLanguageRoot org.eventb.core.accurate="false" org.eventb.core.configuration="org.eventb.theory.core.tul" org.eventb.core.source="/Experiment015_UseTheory/TheoryPath.tul"/>
